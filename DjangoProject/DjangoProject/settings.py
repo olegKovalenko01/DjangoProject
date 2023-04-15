09ac336e174a4bdd99c69193df4b0ce4f198a84b
Oleg Kovalenko <oleggelo1337@gmail.com>
2023-04-15 19:15:04 +0300

    refactor templatetags

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -18,13 +18,13 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
